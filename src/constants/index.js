30dfacea9b009aa4a9d313051fe2277982aed285
John Renzzel <ronztajanlangit09@gmail.com>
2025-08-05 13:41:14 +0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+import project3 from "../assets/projects/project-3.webp";
+import project4 from "../assets/projects/project-4.webp";
+
+export const HERO_CONTENT = `Hi! I'm John Ronald Tajanlangit, a BS Information Technology graduate from Cordova Public College. I’m passionate about technology, programming, and building practical digital solutions that solve everyday problems.
+
+I'm constantly exploring new tools, frameworks, and technologies to sharpen my skills. Currently, I'm focused on expanding my portfolio, collaborating on meaningful projects, and seeking opportunities where I can grow professionally while delivering value through versatile, user-centric solutions.`;
+
+export const ABOUT_TEXT = ``;
+
+export const EXPERIENCES = [
+  {
+    year: "January 21, 2025 – June 1, 2025 ",
+    role: "Full Stack Developer",
+    company: "AmeeraTel Inc",
+    description: `During my internship, I developed a full-stack Workstation Mapping System designed to help IT and admin personnel manage, assign, and monitor workstation locations within an organization.`,
+    technologies: ["Javascript", "PHP", "MySQL"],
+  },
+  {
+    year: "April 11, 2024 – June 10, 2024",
+    role: "Back-end",
+    company: "TESDA Programming Java NC III",
+    description: `Developing program codes using Java technology, including object-oriented analysis and design, and creating and refining Java applications with object-oriented programming concepts as part of a systems development team.`,
+    technologies: ["Java", "DerbyDb"],
+  },
+  {
+    year: "January 22, 2024 – December 22, 2024",
+    role: "Full Stack Developer",
+    company: "Capstone Project",
+    description: `Developed an Online Food Ordering System for our capstone project, enabling real-time menu browsing and order management. Earned the highest panelist score of 95.6 for functionality, UI, and technical quality..`,
+    technologies: ["Next.js", "MongoDb", "React", "Tailwind"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Poblacion, Cordova Cebu ",
+  phoneNo: "+63 9487283977 ",
+  email: "ronztajanlangit09@gmail.com",
+};
