2537907f6e5b1b29188b2bf1e8ed5fef6eb1f7ab
John Renzzel <ronztajanlangit09@gmail.com>
2025-08-05 14:16:57 +0800

    Fixing Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
       <div className="text-center tracking-tighter">
         <motion.p
           whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -500 }}
+          initial={{ opacity: 0, x: -200 }}
           transition={{ duration: 1 }}
           className="my-4"
         >
